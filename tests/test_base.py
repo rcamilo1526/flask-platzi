d9db6bc06b4b2c3d338f147eccd903eddf6ef507
Raúl Camilo Martín Bernal <rcmartinb@correo.udistrital.edu.co>
2021-07-02 13:24:55 -0500

    Blueprint 1.5

--- tests/test_base.py
+++ tests/test_base.py
@@ -26,15 +26,12 @@ class MainTest(TestCase):
         self.assert200(response)
 
     def test_hello_post(self):
-        fake_form = {
-            'username': 'fake',
-            'password': 'fake-password'
-        }
-        response = self.client.post(url_for('hello'), data=fake_form)
+
+        response = self.client.post(url_for('hello'))
 
         # import pdb; pdb.set_trace()
-
-        self.assertRedirects(response, url_for('index'))
+        self.assertTrue(response.status_code, 405)
+        #
 
     def test_auth_blueprint_exists(self):
         self.assertIn('auth', self.app.blueprints)
@@ -46,3 +43,11 @@ class MainTest(TestCase):
     def test_auth_login_template(self):
         self.client.get(url_for('auth.login'))
         self.assertTemplateUsed('login.html')
+
+    def test_auth_login_post(self):
+        fake_form = {
+            'username': 'fake',
+            'password': 'fake-password'
+        }
+        response = self.client.post(url_for('auth.login'), data=fake_form)
+        self.assertRedirects(response, url_for('index'))
