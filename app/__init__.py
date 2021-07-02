2523042d0d1e3eb2d0d2525c970af09ede21c3fa
Camilo <Martn>
2021-07-02 12:23:13 -0500

    config problem solved

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask, request, flash, make_response, redirect, render_template, session, url_for
 from flask_bootstrap import Bootstrap
 
-from config import Config
-
+from .config import Config
+from .auth import auth
 
 def create_app():
     app = Flask(__name__)
