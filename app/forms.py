2d26d6a51e190415ae69a4efea2fa984398b8b20
Raúl Camilo Martín Bernal <rcmartinb@correo.udistrital.edu.co>
2021-07-06 14:26:17 -0500

    Add Todos

--- app/forms.py
+++ app/forms.py
@@ -7,3 +7,7 @@ class LoginForm(FlaskForm):
     username = StringField('Nombre de usuario', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Enviar')
+
+class TodoForm(FlaskForm):
+    description = StringField('Descripción', validators=[DataRequired()])
+    submit = SubmitField('Crear')
--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from flask import request, flash, make_response, redirect, render_template, sess
 from flask_login import login_required, current_user
 
 from app import create_app
-from app.forms import LoginForm
-from app.firestore_service import get_users, get_todos
+from app.forms import TodoForm
+from app.firestore_service import get_users, get_todos, put_todo
 
 app = create_app()
 
@@ -35,16 +35,18 @@ def index():
     return response
 
 
-@app.route('/hello', methods=['GET'])
+@app.route('/hello', methods=['GET', 'POST'])
 @login_required
 def hello():
     user_ip = session.get('user_ip')
     username = current_user.id
+    todo_form = TodoForm()
 
     context = {
         'user_ip': user_ip,
         'todos': get_todos(user_id=username),
-        'username': username
+        'username': username,
+        'todo_form': todo_form
     }
     # users = get_users()
     #
@@ -52,4 +54,11 @@ def hello():
     #     print(user.id)
     #     print(user.to_dict()['password'])
     # return 'Hellos World Platzi, tu IP es: {}'.format(user_ip)
+
+    if todo_form.validate_on_submit():
+        put_todo(user_id=username, description=todo_form.description.data)
+        flash("Tarea creada con exito!")
+
+        return redirect(url_for('hello'))
+
     return render_template('hello.html', **context)
