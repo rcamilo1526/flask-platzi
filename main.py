b7297733b8ed92954cce5d02b59555262e8882c0
Raúl Camilo Martín Bernal <rcmartinb@correo.udistrital.edu.co>
2021-07-06 18:46:56 -0500

    Edit todos

--- main.py
+++ main.py
@@ -3,8 +3,8 @@ from flask import request, flash, make_response, redirect, render_template, sess
 from flask_login import login_required, current_user
 
 from app import create_app
-from app.forms import TodoForm, DeleteTodoForm
-from app.firestore_service import get_users, get_todos, put_todo, delete_todo
+from app.forms import TodoForm, DeleteTodoForm, UpdateTodoForm
+from app.firestore_service import update_todo, get_todos, put_todo, delete_todo
 
 app = create_app()
 
@@ -42,6 +42,7 @@ def hello():
     username = current_user.id
     todo_form = TodoForm()
     delete_form = DeleteTodoForm()
+    update_form = UpdateTodoForm()
 
     context = {
         'user_ip': user_ip,
@@ -49,6 +50,7 @@ def hello():
         'username': username,
         'todo_form': todo_form,
         'delete_form': delete_form,
+        'update_form': update_form,
     }
     # users = get_users()
     #
@@ -71,3 +73,12 @@ def delete(todo_id):
     user_id = current_user.id
     delete_todo(user_id=user_id, todo_id=todo_id)
     return redirect(url_for('hello'))
+
+
+@app.route('/todos/update/<todo_id>/<int:done>', methods=['POST'])
+def update(todo_id, done):
+    user_id = current_user.id
+    update_todo(user_id=user_id, todo_id=todo_id, done=done)
+    # print('DONE', done)
+    return redirect(url_for('hello'))
+    # pass
